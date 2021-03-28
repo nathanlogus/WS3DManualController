997866057bd35fb324781ad05b80df18df5866a4
Nathan Ribeiro <nathanpirral@gmail.com>
2021-03-28 16:51:41 -0300

    Creation of Build Image and Scripts

---
+++ ws3d-0.0.1/bin/start.bat
@@ -0,0 +1,3 @@
+start cmd /k Call ws3d.bat
+timeout 3
+start cmd /k Call WS3DManualControl.bat
---
+++ ws3d-0.0.1/start.bat
@@ -0,0 +1,3 @@
+start cmd /k Call bin/ws3d.bat
+timeout 3
+start cmd /k Call bin/WS3DManualControl.bat
